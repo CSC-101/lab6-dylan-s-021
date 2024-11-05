31fb0fb796f151dd254045996b0e16fa1b848a30
dylan-s-021 <dylanspencer021@gmail.com>
2024-11-04 17:29:37 -0800

    Update lab6_tests.py

--- lab6_tests.py
+++ lab6_tests.py
@@ -64,15 +64,59 @@ class TestCases(unittest.TestCase):
 
 
     # Part 1
+    def test_selection_sort_books_1(self):
+        input = [data.Book(["Suzanne Collins"], "The Hunger Games"),
+                         data.Book(["Lois Lowry"], "The Giver")]
+        expected = [data.Book(["Lois Lowry"], "The Giver"),
+                    data.Book(["Suzanne Collins"], "The Hunger Games")]
+        lab6.selection_sort_books(input)
+        self.assertEqual(expected, input)
 
 
     # Part 2
+    def test_swap_case_1(self):
+        string = "Hello"
+        expected = "hELLO"
+        actual = lab6.swap_case(string)
+        self.assertEqual(expected, actual)
+
+    def test_swap_case_2(self):
+        string = "Hello & gOODBYE"
+        expected = "hELLO & Goodbye"
+        actual = lab6.swap_case(string)
+        self.assertEqual(expected, actual)
 
 
     # Part 3
+    def test_str_translate_1(self):
+        string = "abcdcba"
+        old = "a"
+        new = "x"
+        expected = "xbcdcbx"
+        actual = lab6.str_translate(string, old, new)
+        self.assertEqual(expected, actual)
+
+    def test_str_translate_2(self):
+        string = "test: string w/ characters?"
+        old = "?"
+        new = "!"
+        expected = "test: string w/ characters!"
+        actual = lab6.str_translate(string, old, new)
+        self.assertEqual(expected, actual)
 
 
     # Part 4
+    def test_histogram_1(self):
+        string = "a string with some words"
+        expected = {"a": 1, "string": 1, "with": 1, "some":1, "words":1}
+        actual = lab6.histogram(string)
+        self.assertEqual(expected, actual)
+
+    def test_histogram_2(self):
+        string = "a string with some words & some more words"
+        expected = {"a": 1, "string": 1, "with": 1, "some":2, "words":2, "&":1, "more":1}
+        actual = lab6.histogram(string)
+        self.assertEqual(expected, actual)
 
 
 
